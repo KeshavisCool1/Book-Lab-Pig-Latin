971e300fd63073c2cac2624fb8772cfc7953c69e
KeshavisCool1 <ks3244@pleasantonusd.net>
2025-11-14 20:30:16 -0800

    Comment out output statements in main method
    Commented out translation and Pig-Latin output statements.

--- App.java
+++ App.java
@@ -1,16 +1,16 @@
 class App {
   public static void main(String[] args) {
     Book aBook = new Book();
-    System.out.println(" ");
-    System.out.println("I can now speak in Pig-Latin? WOW!!! I can translate words and sentences for the students at Foothill High School.");
-    System.out.println(aBook.translateSentence("I can now speak in Pig-Latin? WOW!!! I can translate words and sentences for the students at Foothill High School."));
+    // System.out.println(" ");
+    // System.out.println("I can now speak in Pig-Latin? WOW!!! I can translate words and sentences for the students at Foothill High School.");
+    // System.out.println(aBook.translateSentence("I can now speak in Pig-Latin? WOW!!! I can translate words and sentences for the students at Foothill High School."));
     //output : Iyay ancay ownay eakspay inyay Ig-Latinpay? OWway!!! Iyay ancay anslatetray ordsway andyay entencessay orfay ethay udentsstay atyay Oothillfay Ighhay Oolschay.
-    System.out.println(" ");
-    System.out.println(aBook.pigLatin("WOW!!!"));
+    // System.out.println(" ");
 
-    // System.out.println(aBook.pigLatin("hello!"));
-    // System.out.println(aBook.pigLatin("What?!?"));
-    // System.out.println(aBook.pigLatin("Allons-y"));
+
+    aBook.readBook("https://www.gutenberg.org/cache/epub/6130/pg6130.txt");
+    // num of words 196929 (with split)
+    // num of words 192262 (with iteration in the trasnlate word)
 
   }
 }
